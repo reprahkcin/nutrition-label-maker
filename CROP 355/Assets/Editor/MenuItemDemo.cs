9e1c5adada00497d77434e6a302fe6f604344439
reprahkcin <bignickharper@gmail.com>
2019-06-11 11:39:58 -0700

    sync commit

---
+++ CROP 355/Assets/Editor/MenuItemDemo.cs
@@ -0,0 +1,36 @@
+﻿using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuItemDemo : EditorWindow
+{
+    [MenuItem("Edit/Do A Thing")]
+
+    public static void DoAThing()
+    {
+
+        // Create a Canvas
+        GameObject newCanvas = new GameObject("Canvas");
+        Canvas c = newCanvas.AddComponent<Canvas>();
+        c.renderMode = RenderMode.ScreenSpaceOverlay;
+        // Add the standard components to the new canvas
+        newCanvas.AddComponent<CanvasScaler>();
+        newCanvas.AddComponent<GraphicRaycaster>();
+
+        // Create a Panel
+        GameObject panel = new GameObject("TestPanel");
+        // Add the standard components to the new panel
+        panel.AddComponent<CanvasRenderer>();
+        panel.AddComponent<RectTransform>();
+        Image i = panel.AddComponent<Image>();
+        i.color = new Color(0f, 150f, 150f, 255f);
+
+        // Add the panel as a child of the new canvas
+        panel.transform.SetParent(newCanvas.transform, false);
+
+        // Create a primitive
+        GameObject newCube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        // Configure it
+        newCube.transform.position = new Vector3(5, 10, 20);
+    }
+}
