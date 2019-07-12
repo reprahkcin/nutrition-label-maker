0e50669526ba1ae09f72ee6f864efd827a7fa035
reprahkcin <bignickharper@gmail.com>
2019-07-12 07:19:33 -0700

    friday

---
+++ CROP 355/Assets/LabelLogic.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LabelLogic : MonoBehaviour
+{
+    public GameObject servingSizeInputField;
+    private string servingSizeValue;
+    public GameObject servingsPerContainerInputField;
+    private string servingsPerContainerValue;
+    public GameObject caloriesInputField;
+    private string caloriesValue;
+    public GameObject totalFatInputField;
+    private string totalFatValue;
+    public GameObject saturatedFatInputField;
+    private string saturatedFatValue;
+    public GameObject transFatInputField;
+    private string transFatValue;
+    public GameObject cholesterolInputField;
+    private string cholesterolValue;
+    public GameObject sodiumInputField;
+    private string sodiumValue;
+    public GameObject totalCarbsInputField;
+    private string totalCarbsValue;
+    public GameObject dietaryFiberInputField;
+    private string dietaryFiberValue;
+    public GameObject totalSugarsInputField;
+    private string totalSugarsValue;
+    public GameObject addedSugarsInputField;
+    private string addedSugarsValue;
+    public GameObject proteinInputField;
+    private string proteinValue;
+    public GameObject[] inputFields;
+    public string[] inputValues;
+
+
+    public GameObject servingSizeDD;
+    public GameObject servingsPerContainerDD;
+    public GameObject caloriesDD;
+    public GameObject totalFatDD;
+    public GameObject saturatedFatDD;
+    public GameObject transFatDD;
+    public GameObject cholesterolDD;
+    public GameObject sodiumDD;
+    public GameObject totalCarbsDD;
+    public GameObject dietaryFiberDD;
+    public GameObject totalSugarsDD;
+    public GameObject addedSugarsDD;
+    public GameObject proteinDD;
+    public GameObject[] dropdowns;
+
+
+
+    void BuildArrays()
+    {
+        inputFields = new GameObject[]
+        {
+            servingSizeInputField,
+            servingsPerContainerInputField,
+            caloriesInputField,
+            totalFatInputField,
+            saturatedFatInputField,
+            transFatInputField,
+            cholesterolInputField,
+            sodiumInputField,
+            totalCarbsInputField,
+            dietaryFiberInputField,
+            totalSugarsInputField,
+            addedSugarsInputField,
+            proteinInputField
+        };
+
+        dropdowns = new GameObject[]
+        {
+            servingSizeDD,
+            servingsPerContainerDD,
+            caloriesDD,
+            totalFatDD,
+            saturatedFatDD,
+            transFatDD,
+            cholesterolDD,
+            sodiumDD,
+            totalCarbsDD,
+            dietaryFiberDD,
+            totalSugarsDD,
+            addedSugarsDD,
+            proteinDD
+        };
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        BuildArrays();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(servingSizeInputField.GetComponentInChildren<Text>().text);
+        Debug.Log(servingSizeDD.GetComponentInChildren<TextMeshProUGUI>().text);
+    }
+}
---
+++ CROP 355/Assets/Nutrient.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Nutrient : MonoBehaviour
+{
+    public InputField inputField;
+    public Dropdown inputDD;
+    public TextMeshProUGUI outputLabel;
+    public TextMeshProUGUI outputDV;
+
+    private string nutrientValueStr;
+    private string nutrientUnitStr;
+
+    private string nutrientOutputStr = "0";
+    private string nutrientUnitOutputStr = "g";
+    private string dvOutputStr = "";
+
+    private double convertedValue;
+    private double dvInt;
+    private string dvSuffix = "";
+
+    private double dailyValueG = 78;
+    private double dailyValueMG = 7800;
+
+    private string test = "test"; 
+    private void Start()
+    {
+        inputField.GetComponentsInChildren<Text>()[1].text = "0";
+        nutrientValueStr = "0";
+    }
+
+    void GetValues()
+    {
+        if (inputField.GetComponentsInChildren<Text>()[1].text == "")
+        {
+            gameObject.GetComponent<TextMeshProUGUI>().color = Color.red;
+        }
+        else
+        {
+            nutrientValueStr = inputField.GetComponentsInChildren<Text>()[1].text;
+            gameObject.GetComponent<TextMeshProUGUI>().color = Color.white;
+        }
+
+        if (nutrientValueStr != null)
+        {
+            convertedValue = Convert.ToDouble(nutrientValueStr);
+        }
+        nutrientUnitStr = inputDD.GetComponentInChildren<Text>().text;
+    }
+
+    void DoCalculations()
+    {
+        if (nutrientUnitStr == "g")
+        {
+            dvInt = Math.Round(convertedValue / dailyValueG * 100);
+            dvSuffix = "%";
+        }
+
+        if (nutrientUnitStr == "mg")
+        {
+            dvInt = Math.Round(convertedValue / dailyValueMG * 100);
+            dvSuffix = "%";
+        }
+
+    }
+
+    void OutputValues()
+    {
+        nutrientOutputStr = nutrientValueStr + nutrientUnitStr;
+        outputLabel.GetComponentInChildren<TextMeshProUGUI>().text = nutrientOutputStr;
+        dvOutputStr = dvInt + dvSuffix;
+        if (outputDV != null)
+        {
+            outputDV.GetComponentInChildren<TextMeshProUGUI>().text = dvOutputStr;
+        }
+    }
+
+    public void Button()
+    {
+        GetValues();
+        DoCalculations();
+        OutputValues();
+    }
+}
